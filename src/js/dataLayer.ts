11034074759146d765d4fd2572a680edc873f3bf
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-07-21 09:26:21 -0700

    Replace droopy J with Greek Yot, fixes #1

--- src/js/dataLayer.ts
+++ src/js/dataLayer.ts
@@ -191,6 +191,9 @@ export async function getStopTimes(stopId: string): Promise<StopTimes> {
         else {
             lineNum = lineNum.replace(/(BUS|-OWL)$/, '');
         }
+        // Replace droopy J with Greek Yot (non-droopy).
+        // Found here: https://github.com/codebox/homoglyph/blob/763c79a20ba054cc028b3336b5c7b1822db36dc8/raw_data/chars.txt#L48
+        lineNum = lineNum.replaceAll('J', '\u037F');
 
         // Some basic cleanup for lineDest0/1
         let lineDest0 = line.trip_headsign;
